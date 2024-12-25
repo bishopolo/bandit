78f0a8d7857be69269b10f44f393a2461c3b724a
Jorge Echeverri <polo629@protonmail.com>
2024-12-24 19:09:44 -0500

    feat: add bandit solutions

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        verde: "#39EA49",
+      },
+    },
+  },
+  plugins: [],
+};
